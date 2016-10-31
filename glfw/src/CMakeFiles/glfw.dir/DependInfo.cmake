778da86b3b8475f66eba03f8e671b1045c7b1b4c
alpgc <alpgc@cin.ufpe.br>
2016-10-31 13:07:11 -0300

    Pg template

---
+++ glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/llamaphone/Desktop/API/glfw/src/context.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/egl_context.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/glx_context.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/init.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/input.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/linux_joystick.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/monitor.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/posix_time.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/posix_tls.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/posix_tls.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/vulkan.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/window.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/x11_init.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/x11_monitor.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/x11_window.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o"
+  "/home/llamaphone/Desktop/API/glfw/src/xkb_unicode.c" "/home/llamaphone/Desktop/API/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "glfw/include"
+  "glfw/src"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
